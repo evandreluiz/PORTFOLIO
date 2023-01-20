60609a11af4dd11facc619f10f9f376963bf06d4
Andre Luiz <ev.andreluiz2021@yahoo.com>
2023-01-20 17:48:01 -0300

    Atualização do raw do api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/evandreluiz/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/evandreluiz/PORTFOLIO/master/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json() 
 }
